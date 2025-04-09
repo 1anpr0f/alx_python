e5ce76bf50bf60cfe73a9c38ed284ce264619d48
1anpr0f <derickian57@gmail.com>
2025-04-09 11:33:33 +0300

    made changes

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,16 +1,1 @@
-def div(i):
-    hex_chars = "0123456789ABCDEF"
-    result = ""
-    if i == 0:
-        return "0"
-
-    while i > 0:
-        rem = i % 16
-        result = hex_chars[rem] + result  # prepend the character
-        i = i // 16
-
-    return result
-
-# Test for numbers 0–98
-for i in range(99):
-    print(f"{i} => {div(i)}")
+print(f'"".join("0123456789ABCDEF"[int(d)] for d in hex(int(input()))[2:].upper())')
