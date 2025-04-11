805c52fb80819b260d3dbbb9e2d1e44cfcf713b2
1anpr0f <derickian57@gmail.com>
2025-04-12 01:44:34 +0300

    added if statement

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,7 +2,10 @@ def fibonacci_sequence(n):
     empty = []
     a = 0 
     b = 1
-    empty.insert(0,0)
+    if n == 0:
+        return empty
+    else:
+        empty.insert(0.0)
     for _ in range(n-1):
         a,b = b,a+b
         empty.append(a)
