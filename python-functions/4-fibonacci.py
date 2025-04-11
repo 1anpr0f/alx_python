8943142a1fca8209f4d71c9b46aa7695b3236195
1anpr0f <derickian57@gmail.com>
2025-04-12 01:45:03 +0300

    made change

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -5,7 +5,7 @@ def fibonacci_sequence(n):
     if n == 0:
         return empty
     else:
-        empty.insert(0.0)
+        empty.insert(0,0)
     for _ in range(n-1):
         a,b = b,a+b
         empty.append(a)
