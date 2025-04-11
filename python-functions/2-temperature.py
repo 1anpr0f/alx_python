2acf2f2c2850c89f0127984f60ad8d9a92b4f62b
1anpr0f <derickian57@gmail.com>
2025-04-12 00:46:01 +0300

    add

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,6 @@
 def convert_to_celsius(fahrenheit):
     celsius = (5/9) * (fahrenheit-32)
-    if celsius == -celsius:
+    if -celsius == True:
         return round(celsius,2)
     else:
         return celsius
