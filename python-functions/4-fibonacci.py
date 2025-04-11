b23360e830872415f6098573a00eb00b17e55902
1anpr0f <derickian57@gmail.com>
2025-04-12 01:27:34 +0300

    empty

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,7 +1,9 @@
 def fibonacci_sequence(n):
+    empty = []
     a = 0
     b = 1
     for _ in range(n):
         a,b = b,a+b
-        return list(map(str,a))
+        empty.append(a)
+    return empty
     
