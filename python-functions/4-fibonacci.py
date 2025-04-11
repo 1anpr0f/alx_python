17c87277f66b47ac6a763b6670a855c61d8c44d3
1anpr0f <derickian57@gmail.com>
2025-04-12 01:15:49 +0300

    fibnew

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,2 +1,7 @@
 def fibonacci_sequence(n):
-    return(n-1)+(n-2)
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_sequence(n-1)+ fibonacci_sequence(n-2)
