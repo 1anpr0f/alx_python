d9c1b54f88f0b4faa047482d2ad3435b5d825fdb
1anpr0f <derickian57@gmail.com>
2025-04-12 01:29:37 +0300

    a was change

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,8 +1,8 @@
 def fibonacci_sequence(n):
     empty = []
-    a = 0
-    b = 1
     for _ in range(n):
+        a = 0 
+        b = 1
         a,b = b,a+b
         empty.append(a)
     return empty
