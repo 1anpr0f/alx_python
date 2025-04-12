11eaf1c47df780732d8277ca1a5e1524310d9fca
1anpr0f <derickian57@gmail.com>
2025-04-12 03:15:03 +0300

    added cgange

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -4,14 +4,13 @@ def validate_password(password):
     has_upper = False
     if len(password) < 8:
         return False
-    elif len(password) > 8:
-           for i in password:
-               if i.isupper():
-                   has_upper=True
-               if i == " ":
-                   has_space = True
-           if has_upper and not has_space:
-               return True
-           else:
-               return False
-  
+    for i in password:
+        if i.isupper():
+            has_upper = True
+        if i == " ":
+            has_space = True
+
+    if has_upper and not has_space:
+        return True
+    else:
+        return False
