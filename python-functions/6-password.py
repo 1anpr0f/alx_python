c637a4290ce0417a50802e6a6a079ca0b7f977a2
1anpr0f <derickian57@gmail.com>
2025-04-12 03:18:02 +0300

    checked for digits

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,16 +1,24 @@
 
 def validate_password(password):
-    has_space = False
-    has_upper = False
     if len(password) < 8:
         return False
-    for i in password:
-        if i.isupper():
+
+    has_upper = False
+    has_lower = False
+    has_digit = False
+    has_space = False
+
+    for char in password:
+        if char.isupper():
             has_upper = True
-        if i == " ":
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_digit = True
+        elif char == " ":
             has_space = True
 
-    if has_upper and not has_space:
+    if has_upper and has_lower and has_digit and not has_space:
         return True
     else:
         return False
