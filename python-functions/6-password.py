038994bf504ba03b9cc7643075cabd3cf82821eb
1anpr0f <derickian57@gmail.com>
2025-04-12 03:07:04 +0300

    made changes

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,14 +1,18 @@
 
-
 def validate_password(password):
     if len(password) < 8:
         return False
-    elif len(password) >= 8:
-        for i in password:
-            if i.islower():
-                return False
-            elif i == " ":
-                return False
-            return False
-        
+    has_space = False
+    has_upper = False
     
+    for i in password:
+        if i.isupper():
+            has_upper=True
+        if i == " ":
+            has_space = True
+    if has_upper and not has_space:
+        return False
+    else:
+        return True
+    
+  
