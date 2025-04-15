e0f194759dbd8f281f51c5161647d8ff94f47c2a
1anpr0f <derickian57@gmail.com>
2025-04-15 14:03:21 +0300

    change again

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,8 +2,8 @@ import sys
 def main():
     argv = sys.argv[1:]
     count = len(argv)
-    print("{} arguments:".format(count))
+    print("{} argument:".format(count))
     for i,arg in enumerate(argv,start=1):
-        print("{}:{}".format(i,arg))
+        print("{}: {}".format(i,arg))
 if __name__ == "__main__":
     main()
