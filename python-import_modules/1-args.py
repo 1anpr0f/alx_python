3b55e687859e40d9d5100d238c64aa632ec5e9f3
1anpr0f <derickian57@gmail.com>
2025-04-15 14:08:15 +0300

    if statement

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,7 +2,12 @@ import sys
 def main():
     argv = sys.argv[1:]
     count = len(argv)
-    print("{} argument:".format(count))
+    if count == 1:
+        print("{} argument:".format(count))
+    elif count == 0:
+        print("{} arguments:".format(count))
+    else:
+        print("{} arguments:".format(count))   
     for i,arg in enumerate(argv,start=1):
         print("{}: {}".format(i,arg))
 if __name__ == "__main__":
