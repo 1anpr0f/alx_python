7fee720d27a0ce5a8e1c3b6a532842d4546f6d1f
1anpr0f <derickian57@gmail.com>
2025-04-17 18:08:54 +0300

    bug fix

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,7 +1,7 @@
 def no_c(my_string):
     new = ""
-    for i in my_string.lower():
+    for i in my_string:
         for j in i:
-            if j != "c":
+            if j != "c" or j != "C":
                 new+=i
     return new
