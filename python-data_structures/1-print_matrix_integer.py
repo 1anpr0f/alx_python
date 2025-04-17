99fcb0117e4b4b267d20ba3cdcf277609c47112f
1anpr0f <derickian57@gmail.com>
2025-04-17 19:43:32 +0300

    gg

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
+            if i.count(j) == 3:
+                return
             print("{:d}".format(j),end="")
     return matrix
           
