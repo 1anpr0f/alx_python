2fe9d268e82a73f89805f63a1494201def716de1
1anpr0f <derickian57@gmail.com>
2025-04-17 20:19:12 +0300

    df

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,6 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            i.append(int(j))
-        print("{:d}".format(i))
+            print("{:d}".format(i),end=" ")
+        print()
                   
