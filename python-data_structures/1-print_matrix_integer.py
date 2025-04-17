00eeca3c053a2b00b6673744fd8ff7739e5a8fda
1anpr0f <derickian57@gmail.com>
2025-04-17 20:05:10 +0300

    hgf

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            i = "{:d}".format(j)
-        print(i)          
+            i.appnde("{:d}".format(j))
+    print(i)          
