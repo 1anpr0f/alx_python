eccc790fe3a812703b3762e2edae03b2695de3e6
1anpr0f <derickian57@gmail.com>
2025-04-23 12:23:00 +0300

    eer

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,6 +1,4 @@
 def common_elements(set_1, set_2):
-    set_1 = sorted(list(set_1))
-    set_2 = sorted(list(set_1))
-    for k, j in zip(set_1,set_2):
+    for k, j in zip(sorted(set_1),sorted(set_2)):
         if k == j :
             print(list(k))
