8fa42332f9f626c6e5ef5252d05a1a320eea53d3
1anpr0f <derickian57@gmail.com>
2025-04-23 12:33:02 +0300

    eer

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,6 +1,9 @@
 def common_elements(set_1, set_2):
-    for word1, word2 in zip(sorted(set_1),sorted(set_2)):
-        if word1== word2 :
-            return list(word1)
-        else:
-            return list()
+    empty_list = []
+    for word1 in set_1:
+        for word2 in set_2:
+            if word1== word2 :
+                empty_list.append(word1)
+                print(empty_list)
+            else:
+                print(empty_list)
