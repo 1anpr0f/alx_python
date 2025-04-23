7d29fc0cfcb1e8d66e37793db6dee9b707fdfaf8
1anpr0f <derickian57@gmail.com>
2025-04-23 11:21:34 +0300

    eer

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,8 @@ def square_matrix_simple(matrix=[]):
     for i in matrix:
         for  j in i:
             print("{:d}".format(j), end=", " if j != i[-1] else "")
-    print(list(map(lambda x:x**2,i)))
+        print()
+        print(list(map(lambda x:x**2,i)))
         
 
   
