14e8e3f81fd9553f8fd7380b8a0a698a0ea8c726
1anpr0f <derickian57@gmail.com>
2025-04-23 13:28:46 +0300

    eer

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,8 +1,10 @@
 def best_score(a_dictionary):
     z = 0
+    best_key = None
     for k,v in a_dictionary.items():
         if v > z:
             z = v
-        return  "{}".format(k)
+            best_key = k
+    return  "{}".format(best_key) if best_key != None else None
     
         
