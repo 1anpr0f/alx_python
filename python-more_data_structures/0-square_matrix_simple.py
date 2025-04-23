9a05240e229b958dff313375e2615d6faafa09c6
1anpr0f <derickian57@gmail.com>
2025-04-23 11:29:01 +0300

    fgf

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,7 @@ def square_matrix_simple(matrix=[]):
     for i in matrix:
         for  j in i:
           itr = "{:d}".format(j)
-          i = ", ".join(itr)
-        print(list(map(lambda x:x**2,i)))
+        print(list(map(lambda x:x**2,itr)))
         
 
   
