3cb5d9734b9dc2cd197aaa8ca83e8d338f00d82c
1anpr0f <derickian57@gmail.com>
2025-04-23 11:11:21 +0300

    fgfguy

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 def square_matrix_simple(matrix=[]):
     for i in matrix:
-        for  k,j in i:
-            print("{:d}".format(j**2), end=", " if k < len(i)-1 else end="")
+        for  j in i:
+            print("{:d}".format(j**2), end=", " if j != 1[-1] else "")
         print()
 
             
