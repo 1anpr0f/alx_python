93ebeef1c7f76aa8b6ee775283178ae9bddbba67
1anpr0f <derickian57@gmail.com>
2025-04-26 09:53:04 +0300

    fghj

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
 class Square:
+    """Class that defines a square with a private size attribute."""
+
     def __init__(self, size):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square. No type/value verification.
+        """
         self.__size = size
