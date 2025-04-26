bebd073bd6a5f48b82554665c59cd8a74c7098f6
1anpr0f <derickian57@gmail.com>
2025-04-26 09:38:03 +0300

    fgh

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 """creating a class"""
 """difines private instance on"""
-class square:
-    def __init__(self,size):
-        self.__size=size
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
         
