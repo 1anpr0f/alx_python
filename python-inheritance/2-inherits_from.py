45d6a5f6545fe0bc78aea5dc238f8eb32f7c70b9
1anpr0f <derickian57@gmail.com>
2025-05-01 04:24:28 +0300

    changes

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,7 +1,7 @@
 """create a function"""
 def is_kind_of_class(obj, a_class):
-    """retrun true if the objwct is an instance of a clsaa that inherits
-    retrun false if object is direct intanse or unrelated"""
+    """return true if the obj is an instance of a_class that inherits 
+    return false if obj is a direct instance or unrelated""" 
     if isinstance(obj,a_class) and type(obj)!=a_class:
         return True
     return False
