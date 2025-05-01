df8a1f40d936260220ca6b8cd1217e24e6074aae
1anpr0f <derickian57@gmail.com>
2025-05-01 10:45:40 +0300

    news try

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -8,14 +8,4 @@ class BaseGeometry:
     def area(self):
         """Raises an exception with a message for unimplemented area."""
         raise Exception("area() is not implemented")
-if __name__ == "__main__":
-    bg=BaseGeometry()
-    if isinstance (bg,BaseGeometry):
-        attributes = [attr for attr in dir(bg) if not attr.startswith('__') or attr == 'area']
-        print(attributes)
-        
-        try:
-            bg.area()
-        except Exception as e:
-            print("[{}] {}".format(type(e).__name__, e))
-     
+
