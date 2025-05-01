2f46243b73cb377660d6a48363e0310da24be0c7
1anpr0f <derickian57@gmail.com>
2025-05-01 05:21:47 +0300

    newa\h

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Defines a function that checks object type inheritance."""
-  
-  
-def is_kind_of_class(obj, a_class):
-    '''
-    Check if an object is an instance of, or inherits from, a specified class.
+"""Defines a function that checks for inherited class instances."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if an object is an instance of a subclass of a_class.
 
     Args:
         obj: The object to check.
         a_class: The class to compare against.
 
     Returns:
-        bool: True if obj is an instance of a_class or a subclass of a_class,
-              False otherwise.
-    '''
-    return isinstance(obj, a_class)
+        bool: True if obj is an instance of a subclass of a_class,
+              False if obj is a direct instance or unrelated.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
