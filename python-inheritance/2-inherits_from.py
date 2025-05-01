0fd59bdaa462bcf9aaf6d6031e8152edab3a001f
1anpr0f <derickian57@gmail.com>
2025-05-01 04:26:48 +0300

    changes

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,7 +1,9 @@
 """create a function"""
 def is_kind_of_class(obj, a_class):
-    """return true if the obj is an instance of a_class that inherits 
-    return false if obj is a direct instance or unrelated""" 
-    if isinstance(obj,a_class) and type(obj)!=a_class:
-        return True
-    return False
+    """
+    Return True if the object is an instance of a class that inherits
+    from the specified class (a_class), either directly or indirectly.
+
+    Return False if the object is a direct instance of a_class or is unrelated.
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
