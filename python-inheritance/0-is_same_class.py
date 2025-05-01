197767894b090827d87b7d174713f7158b468e42
1anpr0f <derickian57@gmail.com>
2025-05-01 03:56:10 +0300

    documented

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,4 +1,8 @@
+"""create function"""
 def is_same_class(obj, a_class):
+    """check if function is exact 
+    instance of a class and return true else 
+     return false"""
     if type(obj) == a_class:
         return True
     return False
