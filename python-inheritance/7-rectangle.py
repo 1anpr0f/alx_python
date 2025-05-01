d3a13fd0cb1b00753900677ee60b5e8302f3e7be
1anpr0f <derickian57@gmail.com>
2025-05-01 09:48:10 +0300

    impliment area

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,21 @@
+"""read the file from 5-base_geometry.py"""
+with open('5-base_geometry.py','r') as file:
+    code=file.read()
+    exec(code)
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle, inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initializes the rectangle with width and height, validates them."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the string representation of the Rectangle."""
+        return f"[Rectangle] {self.__width} /{self.__height}"
