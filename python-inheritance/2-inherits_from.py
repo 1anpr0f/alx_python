b8d7ae2746bf427931f46dac7ed3982623d25acd
1anpr0f <derickian57@gmail.com>
2025-05-01 04:45:10 +0300

    newa

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,6 +1,11 @@
 """create a function"""
 def is_kind_of_class(obj, a_class):
-    """check if object is instance of subclass"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    """ return true if object isinstance of subclass.
+    
+    else return false
+    """
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    return False
     
 
