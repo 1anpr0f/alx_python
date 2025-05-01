fb984a289a80f99d5e9d24b4922b3c2e2423cfa9
1anpr0f <derickian57@gmail.com>
2025-05-01 05:00:04 +0300

    newa

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,17 +1,18 @@
 """create a function."""
 def is_kind_of_class(obj, a_class):
+    
     """
-        Determine if an object is an instance of a subclass of the specified class.
+    Determine if an object is an instance of a subclass of the specified class.
 
-        This function returns True if `obj` is an instance of a class that inherits
-        (directly or indirectly) from `a_class`, but is not a direct instance of `a_class`.
+    This function returns True if `obj` is an instance of a class that inherits
+    (directly or indirectly) from `a_class`, but is not a direct instance of `a_class`.
 
-        Args:
-            obj (Any): The object to check.
-            a_class (type): The class to compare inheritance against.
+    Args:
+        obj (Any): The object to check.
+        a_class (type): The class to compare inheritance against.
 
-        Returns:
-            bool: True if `obj` is an instance of a subclass of `a_class`, False otherwise.
+    Returns:
+        bool: True if `obj` is an instance of a subclass of `a_class`, False otherwise.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
 
