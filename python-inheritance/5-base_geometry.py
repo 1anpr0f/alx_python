415c1cd70d54fdc7645bd565a65618b96b13c8b9
1anpr0f <derickian57@gmail.com>
2025-05-01 05:59:05 +0300

    validate

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,6 +4,8 @@
 
 class BaseGeometry:
     """A base class for geometric shapes."""
+    def __init__(self):
+        pass
 
     def area(self):
         """Raises an exception with a message for unimplemented area."""
