32b1d545be685a76be12f66f492123207c18f10e
1anpr0f <derickian57@gmail.com>
2025-05-01 05:38:01 +0300

    baseclass

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,8 +1,10 @@
-"""create a class BaseGeometry."""
+#!/usr/bin/python3
+"""Defines the BaseGeometry class with an area method."""
+
+
 class BaseGeometry:
     """A base class for geometric shapes."""
+
     def area(self):
         """Raises an exception with a message for unimplemented area."""
         raise Exception("area() is not implemented")
-    
-        
