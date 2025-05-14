debd5209ee11f33f08ff11315a3f5546c205ec40
1anpr0f <derickian57@gmail.com>
2025-05-14 08:57:26 +0300

    request

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,6 @@
-import urllib.request
-# import urllib.parse
-url ='https://alu-intranet.hbtn.io/status'
-"""This line creates a Request object for the given url. But it does NOT actually send the request yet."""
-response = urllib.request.Request(url)
-"""send the request"""
-resp=urllib.request.urlopen(response)
-respdata = resp.read()#reads the response
-
-print(respdata)
+import requests
+url='https://alu-intranet.hbtn.io/status'
+#	Retrieve data
+the_data = requests.get(url)
+print(the_data.text)
+print(the_data.status_code)
