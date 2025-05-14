42047bb804746cea06bec7d529b5bd899f308055
1anpr0f <derickian57@gmail.com>
2025-05-14 09:24:28 +0300

    post

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,5 +3,6 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     e_mail=sys.argv[2]
-    respose=requests.post(url,data=e_mail)
+    data={'data':e_mail}
+    respose=requests.post(url,data=data)
     print(respose.text)
