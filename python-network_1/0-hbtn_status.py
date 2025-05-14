e1c2bc3b6d7c827971827bdcc000d819ba0f72dc
1anpr0f <derickian57@gmail.com>
2025-05-14 09:02:12 +0300

    request

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,5 +2,6 @@ import requests
 url='https://alu-intranet.hbtn.io/status'
 #	Retrieve data
 the_data = requests.get(url)
-print(the_data)
-
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
