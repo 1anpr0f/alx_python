b01c050eaf31e00ff5d29d159d7a64b40453e307
1anpr0f <derickian57@gmail.com>
2025-05-14 09:47:28 +0300

    git

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,12 +1,19 @@
+#!/usr/bin/python3
 import requests
 import sys
-if __name__  == '__main__':
-    url='http://0.0.0.0:5000/search_user'
-    q = sys.argv[1] if len(sys.argv[1]) > 1 else ''
-    data={'q':q}
+
+if __name__ == "__main__":
+    # Get the letter from command-line arguments or use an empty string
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    # Send the POST request with q as a parameter
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': q}
+
     try:
-        response=requests.post(url,data=data)
+        response = requests.post(url, data=data)
         json_data = response.json()
+
         if json_data:
             print(f"[{json_data.get('id')}] {json_data.get('name')}")
         else:
