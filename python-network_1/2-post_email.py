be63bc0373ad2d08c56c46a01af229c755d64509
1anpr0f <derickian57@gmail.com>
2025-05-14 09:27:56 +0300

    git

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,7 +2,7 @@ import requests
 import sys
 if __name__ == '__main__':
     url = sys.argv[1]
-    e_mail=sys.argv[2]
-    data={'data':e_mail}
-    respose=requests.post(url,data=data)
-    print(respose.text)
+    email=sys.argv[2]
+    data = {'email': email}  # Send with key 'email'
+    response = requests.post(url, data=data)
+    print(response.text)
