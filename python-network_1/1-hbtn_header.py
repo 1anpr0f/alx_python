9f6ae5604f951c5186d2beee1aa70979d6f86083
1anpr0f <derickian57@gmail.com>
2025-05-14 09:15:44 +0300

    request

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,4 +3,4 @@ import sys
 if __name__ == '__main__':
     url=sys.argv[1]
     the_data=requests.get(url)
-    print(the_data.request.get('X-Request-Id'))
+    print(the_data.headers.get('X-Request-Id'))
